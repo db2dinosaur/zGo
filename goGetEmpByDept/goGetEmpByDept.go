0aa82a10dab5e92c101c4e592d528346014d0fba
James <james.gill@triton.co.uk>
2022-06-06 17:15:31 +0100

    goGetEmpByDept

---
+++ goGetEmpByDept/goGetEmpByDept.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bytes"
+	b64 "encoding/base64"
+	json "encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	jsonReq := []byte(`{
+		"mgr": "000010",
+		"dept":"A00"
+	}`)
+
+	/* Use the NewRequest interface to allow us to set all of the headers */
+	url := "http://192.168.225.225:5040/services/GILLJ/GetEmployeesByDepartment"
+	var auth = "Basic " + b64.StdEncoding.EncodeToString([]byte("GILLJ:sausage"))
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonReq))
+	req.Header.Add("Authorization", auth)
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accept", "application/json")
+
+	// Send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("Error while reading the response bytes:", err)
+	}
+	//	log.Println(string(body))
+
+	var jsonMap map[string]interface{}
+	json.Unmarshal([]byte(body), &jsonMap)
+	var statusCode = jsonMap["StatusCode"].(float64)
+	stc := int(statusCode)
+	if stc == 200 {
+		log.Println("--- OKAY ---")
+		rj, err := json.MarshalIndent(jsonMap, "", "  ")
+		if err != nil {
+			log.Fatal("Error marshalling JSON : ", err)
+			os.Exit(11)
+		} else {
+			log.Println(string(rj))
+		}
+	} else {
+		log.Fatal(">>> FAIL <<<\nStatusCode = ", stc)
+		os.Exit(12)
+	}
+}
