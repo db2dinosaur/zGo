48337f2920998f6126bc4ca7b10f7ebe046fabd1
db2dinosaur <james.gill@triton.co.uk>
2022-07-04 07:43:16 +0100

    Create goweb.go

---
+++ go web/goweb.go
@@ -0,0 +1,15 @@
+package main
+
+include (
+  "fmt"
+  "net/http"
+  "time"
+)
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+    })
+
+    http.ListenAndServe(":9999", nil)
+}
