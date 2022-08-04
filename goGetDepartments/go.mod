33088105cf4d902c43c05d7ba2ad6fb64f3d84e9
James <james.gill@triton.co.uk>
2022-08-04 12:06:52 +0100

    jsontinker

---
+++ goGetDepartments/go.mod
@@ -0,0 +1,3 @@
+module D:\\Adata\\code\\go\\bin\\goGetDepartments
+
+go 1.17
---
+++ jsontinker/jsontinker.go
@@ -0,0 +1,30 @@
+import (
+	json "encoding/json"package jsontinker
+	"fmt"
+)
+
+func main() {
+	string fname = "config.json"
+	var jsonMap map[string]interface{}
+	pdata, err = ioutil.ReadFile(fname)
+	if err != nil {
+		fmt.Println("ReadFile error : ",err)
+	} else {
+		err = json.Unmarshall([]byte(pdata), &jsonMap)
+		if err != nil {
+			fmt.Println("Unmarshall error : ",err)
+		} else {
+			userid = jsonMap["userid"]
+			password = jsonMap["password"]
+			host = jsonMap["host"]
+			port = jsonMap["port"]
+			dbname = jsonMap["dbname"]
+		}
+	}
+	fmt.Println("Values retrieved from ",fname)
+	fmt.Println("userid   = ",userid)
+	fmt.Println("password = ",password)
+	fmt.Println("host     = ",host)
+	fmt.Println("port     = ",port)
+	fmt.Println("dbname   = ",dbname)
+}
